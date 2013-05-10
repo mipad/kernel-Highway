1cd9c8ecd0023890ef04a0252df2364cef983f56
Stephen Smalley <sds@tycho.nsa.gov>
2013-05-10 10:16:19 -0400

    mocha:Enable setting security contexts on rootfs inodes.

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -378,6 +378,15 @@ static int sb_finish_set_opts(struct super_block *sb)
 	    !strcmp(sb->s_type->name, "debugfs") ||
 	    !strcmp(sb->s_type->name, "rootfs"))
 		sbsec->flags |= SE_SBLABELSUPP;
+
+	/*
+	 * Special handling for rootfs. Is genfs but supports
+	 * setting SELinux context on in-core inodes.
+	 */
+	if (strncmp(sb->s_type->name, "rootfs", sizeof("rootfs")) == 0)
+		sbsec->flags |= SE_SBLABELSUPP;
+
+
 	/* Initialize the root inode. */
 	rc = inode_doinit_with_dentry(root_inode, root);
 	/* Initialize any other inodes associated with the superblock, e.g.
