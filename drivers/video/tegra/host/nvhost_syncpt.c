40bb231c33aa69cfdf295da2615bea6f05b47a30
Gagan Grover <ggrover@nvidia.com>
2016-11-22 15:43:19 +0530

    moc:video: tegra: host: use lock to get syncpt name

--- drivers/video/tegra/host/nvhost_syncpt.c
+++ drivers/video/tegra/host/nvhost_syncpt.c
@@ -3,8 +3,8 @@
  *
  * Tegra Graphics Host Syncpoints
  *
- * Copyright (c) 2010-2014, NVIDIA CORPORATION. All rights reserved.
- * Copyright (C) 2016 XiaoMi, Inc.
+ * Copyright (c) 2010-2018, NVIDIA CORPORATION. All rights reserved.
+
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
